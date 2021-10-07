c72f781fc6eab53e72d33d183e59e3f437b354bd
Rushikesh <62871524+scardam144@users.noreply.github.com>
2021-10-07 16:27:48 +0530

    the costomuserdetailsapp file

--- src/main/java/com/example/demo/CustomUserDetails.java
+++ src/main/java/com/example/demo/CustomUserDetails.java
@@ -55,5 +55,8 @@ public class CustomUserDetails implements UserDetails {
     public String getFullName() {
         return user.getFirstName() + " " + user.getLastName();
     }
+    public String getUserEmail() {
+        return user.getEmail();
+    }
  
 }
